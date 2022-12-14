09dc3b4236413426d8a93d6a0ec640140222153e
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-12-14 23:53:11 +0300

    feat: add filtering logic

---
+++ src/components/view/elements/searchInput.ts
@@ -0,0 +1,6 @@
+import { el } from 'redom';
+// import { router } from '../../router';
+
+export const searchInput: HTMLElement = el('input.catalog__search', {
+  placeholder: 'Search product...',
+});
