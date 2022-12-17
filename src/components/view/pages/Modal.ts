a760286d75f75a15411b936642af9f829a94fa8f
Alexandra Samoilova <avsamoilava@gmail.com>
2022-12-17 23:27:40 +0300

    fix: correct class names in modal render

--- src/components/view/pages/Modal.ts
+++ src/components/view/pages/Modal.ts
@@ -1,30 +1,27 @@
 import { el } from 'redom';
 export class Modal {
-  render() {
+  render(): HTMLElement {
     return el('.modal', [
-      el('.modal__content', [
+      el('.modal__title', 'Personal details'),
+      el('.modal__close', ''),
+      el(
+        'form.modal__form.form',
+        el('.form__inputs', [
+          el('input.form__name', { type: 'text', placeholder: 'enter your full name' }),
+          el('input.form__phone', { type: 'tel', placeholder: 'enter your phone number' }),
+          el('input.form__address', { type: 'text', placeholder: 'enter delivery address' }),
+          el('input.form__email', { type: 'email', placeholder: 'enter your email' }),
+        ]),
         el(
-          'form.modal__form.form',
-          el(
-            '.form__wrapper',
-            el('.form__inputs', [
-              el('input.form__name', { type: 'text', placeholder: 'enter your full name' }),
-              el('input.form__phone', { type: 'tel', placeholder: 'enter your phone number' }),
-              el('input.form__address', { type: 'text', placeholder: 'enter delivery address' }),
-              el('input.form__email', { type: 'email', placeholder: 'enter your email' }),
-            ]),
-            el(
-              '.form__card.card',
-              el('.card__wrapper', [
-                el('input.card__num', { type: 'number', placeholder: '0000 0000 0000 0000' }),
-                el('input.card__valid', { type: 'number', placeholder: '00/00' }),
-                el('input.card__cvv', { type: 'number', placeholder: '000' }),
-              ])
-            ),
-            el('button.form__btn.btn.btn-fill', 'CONFIRM')
-          )
+          '.form__card.credit',
+          el('.credit__wrapper', [
+            el('input.credit__num', { type: 'number', placeholder: '0000 0000 0000 0000' }),
+            el('input.credit__valid', { type: 'number', placeholder: '00/00' }),
+            el('input.credit__cvv', { type: 'number', placeholder: '000' }),
+          ])
         ),
-      ]),
+        el('button.form__btn.btn.btn-fill', 'CONFIRM')
+      ),
     ]);
   }
 }
