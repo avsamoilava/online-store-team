7136a472448c743494a671e43b42629a7b6796b2
Alexandra Samoilova <avsamoilava@gmail.com>
2022-12-19 22:14:56 +0300

    feat: add payment system type render when card number is entered

--- src/components/view/pages/Modal.ts
+++ src/components/view/pages/Modal.ts
@@ -1,11 +1,44 @@
 import { el, setChildren } from 'redom';
 import IMask from 'imask';
+import maestro from '../../../assets/images/icons/maestro.png';
+import mir from '../../../assets/images/icons/mir.png';
+import amex from '../../../assets/images/icons/amex.png';
+import jcb from '../../../assets/images/icons/jcb.png';
+import dinersClub from '../../../assets/images/icons/diners-club.png';
+import visa from '../../../assets/images/icons/visa.png';
+import masterCard from '../../../assets/images/icons/mastercard.png';
+import union from '../../../assets/images/icons/unionpa.png';
+import discover from '../../../assets/images/icons/discover.png';
+import placeholder from '../../../assets/images/icons/card-placeholder.png';
 
 export class Modal {
   private modalWrap = el('.cart__modal');
-  private cardNum = el('input.credit__num', { type: 'text', placeholder: '0000 0000 0000 0000' });
-  private year = el('input.credit__valid', { type: 'text', placeholder: '00/00' });
-  private code = el('input.credit__cvv', { type: 'text', placeholder: '000' });
+  private form = {
+    fullName: el('input.form__name', {
+      type: 'text',
+      placeholder: 'enter your full name',
+    }) as HTMLInputElement,
+    phoneNum: el('input.form__phone', {
+      type: 'tel',
+      placeholder: 'enter your phone number',
+    }) as HTMLInputElement,
+    address: el('input.form__address', {
+      type: 'text',
+      placeholder: 'enter delivery address',
+    }) as HTMLInputElement,
+    email: el('input.form__email', {
+      type: 'email',
+      placeholder: 'enter your email',
+    }) as HTMLInputElement,
+    cardNum: el('input.credit__num', {
+      type: 'text',
+      placeholder: '0000 0000 0000 0000',
+    }) as HTMLInputElement,
+    year: el('input.credit__valid', { type: 'text', placeholder: '00/00' }) as HTMLInputElement,
+    code: el('input.credit__cvv', { type: 'text', placeholder: '000' }) as HTMLInputElement,
+    confirm: el('button.form__btn.btn.btn-fill', 'CONFIRM') as HTMLInputElement,
+  };
+  private payIcon = el('.credit__pay-icon');
 
   render(): HTMLElement {
     this.modalWrap.addEventListener('click', (e) => this.close(e));
@@ -16,27 +49,86 @@ export class Modal {
         el(
           'form.modal__form.form',
           el('.form__inputs', [
-            el('input.form__name', { type: 'text', placeholder: 'enter your full name' }),
-            el('input.form__phone', { type: 'tel', placeholder: 'enter your phone number' }),
-            el('input.form__address', { type: 'text', placeholder: 'enter delivery address' }),
+            this.form.fullName,
+            el('span'),
+            this.form.phoneNum,
+            el('span'),
+            this.form.address,
+            el('span'),
             el('input.form__email', { type: 'email', placeholder: 'enter your email' }),
           ]),
           el(
             '.form__card.credit',
             el('.credit__wrapper', [
-              this.setMask(this.cardNum, '0000 0000 0000 0000'),
-              this.setMask(this.year, '00/00'),
-              this.setMask(this.code, '000'),
+              this.setMask(this.form.cardNum, '0000 0000 0000 0000'),
+              this.setMask(this.form.year, '00/00'),
+              this.setMask(this.form.code, '000'),
+              this.payIcon,
             ])
           ),
-          el('button.form__btn.btn.btn-fill', 'CONFIRM')
+          this.form.confirm
         ),
       ]),
     ]);
     return this.modalWrap;
   }
 
+  private cardInputHandler(): void {
+    this.form.cardNum.addEventListener('blur', () => {
+      console.log(this.form.cardNum.value);
+    });
+    this.form.cardNum.addEventListener('input', () => {
+      console.log(this.checkNum(this.form.cardNum.value));
+      this.setPayIcon(this.form.cardNum.value);
+    });
+  }
+
+  private setPayIcon(val: string): void {
+    if (this.checkNum(val)) {
+      const img = el('img', { src: this.checkNum(val) });
+      setChildren(this.payIcon, [img]);
+    }
+  }
+
+  private checkNum(v: string): string {
+    if (v.slice(0, 1) === '2') return mir;
+    else if (v.slice(0, 1) === '4') return visa;
+    else {
+      switch (v.slice(0, 2)) {
+        case '30':
+        case '36':
+        case '38':
+          return dinersClub;
+        case '31':
+        case '35':
+          return jcb;
+        case '34':
+        case '37':
+          return amex;
+        case '50':
+        case '56':
+        case '57':
+        case '58':
+        case '63':
+        case '67':
+          return maestro;
+        case '51':
+        case '52':
+        case '53':
+        case '54':
+        case '55':
+          return masterCard;
+        case '60':
+          return discover;
+        case '62':
+          return union;
+      }
+    }
+    return placeholder;
+  }
+
   show() {
+    this.cardInputHandler();
     window.scrollTo(0, 0);
     document.body.style.overflow = 'hidden';
     if (!this.modalWrap.classList.contains('cart__modal_active')) {
