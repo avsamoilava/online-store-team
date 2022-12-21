0d34472091065381595970b210c97f9d2701fd9d
Alexandra Samoilova <avsamoilava@gmail.com>
2022-12-21 22:22:59 +0300

    feat: correct updateAmount

--- src/components/utils/updateCart.ts
+++ src/components/utils/updateCart.ts
@@ -7,17 +7,18 @@ export const amountElement = el('.order__sum', `Total cost: 0.00€`);
 export const countElement = el('.order__amount', `Amount: 0`);
 export const promoDiscountAmountElement = el('.order__discount-sum', ``);
 
-export function updateAmount(promo?: number) {
+export function updateAmount() {
+  const discount = JSON.parse(localStorage.getItem('discount') || 'null');
   const products = getProductsInCart();
   const productsCount = getProductsCount(products);
   const totalAmount = +getTotalAmount(products).toFixed(2);
-  const promoAmount = promo ? totalAmount - totalAmount * (0.01 * promo) : null;
+  const promoAmount = discount ? totalAmount - totalAmount * (0.01 * discount) : null;
   headerAmountElement.textContent = `Total cost: ${totalAmount}€`;
   headerCountElement.textContent = `${productsCount}`;
   amountElement.textContent = `Total cost: ${totalAmount}€`;
   countElement.textContent = `Amount: ${productsCount}`;
-  promoDiscountAmountElement.textContent = promo
+  promoDiscountAmountElement.textContent = discount
     ? `Total cost width promo: ${promoAmount?.toFixed(2)}€`
     : '';
-  amountElement.className = !promo ? 'order__sum' : 'order__sum order__sum_cross';
+  amountElement.className = !discount ? 'order__sum' : 'order__sum order__sum_cross';
 }
