fdaf124c81882859fd796fe53bb75af36b4df550
Alexandra Samoilova <avsamoilava@gmail.com>
2022-12-21 20:37:42 +0300

    feat: correct updateCart for render with promocodes

--- src/components/utils/updateCart.ts
+++ src/components/utils/updateCart.ts
@@ -5,13 +5,19 @@ export const headerAmountElement = el('.header__total', `Total amount: 0.00€`)
 export const headerCountElement = el('span.header__cart-count', '0');
 export const amountElement = el('.order__sum', `Total cost: 0.00€`);
 export const countElement = el('.order__amount', `Amount: 0`);
+export const promoDiscountAmountElement = el('.order__discount-sum', ``);
 
-export function updateAmount() {
+export function updateAmount(promo?: number) {
   const products = getProductsInCart();
   const productsCount = getProductsCount(products);
-  const totalAmount = getTotalAmount(products).toFixed(2);
+  const totalAmount = +getTotalAmount(products).toFixed(2);
+  const promoAmount = promo ? totalAmount - totalAmount * (0.01 * promo) : null;
   headerAmountElement.textContent = `Total cost: ${totalAmount}€`;
   headerCountElement.textContent = `${productsCount}`;
   amountElement.textContent = `Total cost: ${totalAmount}€`;
   countElement.textContent = `Amount: ${productsCount}`;
+  promoDiscountAmountElement.textContent = promo
+    ? `Total cost width promo: ${promoAmount?.toFixed(2)}€`
+    : '';
+  amountElement.className = !promo ? 'order__sum' : 'order__sum order__sum_cross';
 }
