96659a8dfcaa4f6c777a999560146a24d65fc553
Alexandra Samoilova <avsamoilava@gmail.com>
2023-01-02 20:19:52 +0300

    fix: correct validateCardNumValue function

--- src/components/utils/validation.ts
+++ src/components/utils/validation.ts
@@ -15,7 +15,8 @@ export const checkEmailValue = (value: string): boolean => {
   return reg.test(value);
 };
 export const validateCardNumValue = (value: string): boolean => {
-  return value.split(' ').join('').length === 16;
+  const cutStr = value.replace(/ /g, '');
+  return cutStr.length === 16 && cutStr.split('').every((char) => !isNaN(Number(char)));
 };
 export const validateCardYearValue = (value: string): boolean => {
   return Number(value.split('/')[0]) <= 12 && Number(value.split('/')[0]) >= 1;
